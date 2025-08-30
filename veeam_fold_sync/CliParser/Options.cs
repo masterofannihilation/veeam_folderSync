d4ceba36e93adb98562e19d861ca4bc1e1e9b988
Boris Hatala <borys.hatala@gmail.com>
2025-08-30 15:58:06 +0200

    feat: cli arg parsing

---
+++ veeam_fold_sync/CliParser/Options.cs
@@ -0,0 +1,38 @@
+﻿using System.Security.AccessControl;
+using CommandLine;
+
+namespace veeam_fold_sync.CliParser;
+
+public class Options
+{
+    [Option('s', "srcFolder", Required = true, HelpText = "Path to the source folder to be replicated") ]
+    public required String SrcFolder { get; set; }
+    
+    [Option('r', "repFolder", Required = true, HelpText = "Path to the replica folder") ]
+    public required String RepFolder { get; set; }
+    
+    [Option('i', "syncInterval", Required = true, HelpText = "Synchronization interval in milliseconds") ]
+    public required int SyncInterval { get; set; }
+    
+    [Option('l', "logFile", Required = true, HelpText = "Path to the log file") ]
+    public required String LogFile { get; set; }
+
+    public bool Validate()
+    {
+        bool srcValid = Directory.Exists(SrcFolder);
+        bool repValid = Directory.Exists(RepFolder);
+        bool logValid = File.Exists(LogFile);
+
+        if (srcValid && repValid && logValid)
+        {
+            Console.WriteLine("Folder paths and file path are valid");
+            return true;
+        }
+
+        Console.WriteLine("Folder path or file path nonexistent");
+        Console.WriteLine($"Source folder: {srcValid}");
+        Console.WriteLine($"Rep folder: {repValid}");
+        Console.WriteLine($"Log file: {logValid}");
+        return false;
+    }
+}
---
+++ veeam_fold_sync/Program.cs
@@ -0,0 +1,31 @@
+﻿using CommandLine;
+using veeam_fold_sync.CliParser;
+
+class Program
+{
+    private static void Main(string[] args)
+    {
+        Parser.Default.ParseArguments<Options>(args)
+            .WithParsed(options =>
+            {
+                if (options.Validate())
+                {
+                    Run(options);
+                }
+            })
+            .WithNotParsed(HandleErrors);
+    }
+
+    static void Run(Options options)
+    {
+        Console.WriteLine("Hello world!");
+    }
+    
+    private static void HandleErrors(IEnumerable<Error> errors)
+    {
+        foreach (var error in errors)
+        {
+            Console.WriteLine(error.ToString());
+        }
+    }
+}
